4a2d5b2a330aafd8c4a7ebd59ad1066869235170
Mali Michael <malimccalla@gmail.com>
2016-06-19 11:45:16 +0100

    Add car project and remove test file

---
+++ car_project/init.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require_relative 'lib/car'
+
+puts
+puts "Enter the information for the car you want to purchase"
+puts
+
+print "Make: "
+make = gets.chomp
+
+print "Year: "
+year = gets.chomp
+
+print "Color: "
+color = gets.chomp
+
+car = Car.new(make: make, year: year, color: color)
+
+puts
+puts "I understand you want to purchase: #{car.full_name}"
+puts
