aa4ddc2da8bf2fa7429107ab4ff7d4fc41dfd931
Mali Michael <malimccalla@gmail.com>
2016-07-09 17:17:25 +0100

    Add doubles_spec.rb

---
+++ car_project/spec/doubles_spec.rb
@@ -0,0 +1,30 @@
+describe 'Doubles' do
+
+  it 'allows stubbing methods' do
+    dbl = double("Chant")
+    allow(dbl).to receive(:hey!)
+    expect(dbl).to respond_to(:hey!)
+  end
+
+  it 'allows stubbing a response with a block' do
+    dbl = double("Chant")
+    allow(dbl).to receive(:hey!) {"Ho!"} #when i say hey you say ho!
+    expect(dbl.hey!).to eq("Ho!")
+  end
+
+  it 'allows stubbing responses with #and_return' do
+    dbl = double("Chant")
+    allow(dbl).to receive(:hey!).and_return("Ho!")
+  end
+
+  it 'allows stubbing multiple responses with #and_return' do
+    die = double("Die")
+    allow(die).to receive(:roll).and_return(1,5,2,6)
+    expect(die.roll).to eq(1)
+    expect(die.roll).to eq(5)
+    expect(die.roll).to eq(2)
+    expect(die.roll).to eq(6)
+    expect(die.roll).to eq(6) #continues returning last value
+  end
+
+end
