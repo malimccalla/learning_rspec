ef7967fab3374350ed6d6efc75b799f2980509c8
Mali Michael <malimccalla@gmail.com>
2016-07-09 12:22:49 +0100

    Add numeric comparison matchers

--- car_project/spec/expectation_matchers_spec.rb
+++ car_project/spec/expectation_matchers_spec.rb
@@ -68,4 +68,24 @@ describe 'Expectation Matchers' do
 
   end
 
+  describe 'numeric comparision matchers' do
+
+    it 'will match less than/greater than' do
+      expect(10).to be > 9
+      expect(10).to be < 11
+      expect(10).to be <= 10
+      expect(10).to be >= 10
+    end
+
+    it 'will match numberic ranges' do
+      expect(10).to be_between(5,10).inclusive
+      expect(10).not_to be_between(5,10).exclusive
+      expect(10).to be_within(1).of(11)
+      expect(5..10).to cover(10)
+      expect(5...10).not_to cover(10)
+    end
+
+  end
+
+
 end
