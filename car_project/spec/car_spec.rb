ad9b2e20a73b3886404b4e4b7dd20c3a342eeeeb
Mali Michael <malimccalla@gmail.com>
2016-07-09 17:05:12 +0100

    use Car class instead of string to set subject implicitly

--- car_project/spec/car_spec.rb
+++ car_project/spec/car_spec.rb
@@ -1,33 +1,38 @@
 require 'car'
 
-describe 'Car' do
+describe Car do #subject will be Car if we use class instead of string
 
   describe 'attributes' do
 
-    let(:car) { Car.new } #when car is used its calling the helper method car which runs that block of code
-    # let is bbetter than before for setting up instance variables
+    # subject {Car.new}
+    # use 'subject' instead of 'let' if variable is subject of example
+
+    # let(:car) { Car.new }
+    #when car is used its calling the helper method car which runs that block of code
+
+    # let is better than before for setting up instance variables
     # before(:example) do
     #   @car = Car.new
     # end
 
     it "allows reading and writing for :make" do
-      car.make = 'Test'
-      expect(car.make).to eq('Test')
+      subject.make = 'Test'
+      expect(subject.make).to eq('Test')
     end
 
     it "allows reading and writing for :year" do
       #skip("Testing skipping this test.")
-      car.year = 9999
-      expect(car.year).to eq(9999)
+      subject.year = 9999
+      expect(subject.year).to eq(9999)
     end
 
     it "allows reading and writing for :color" do
-      car.color = "foo"
-      expect(car.color).to eq("foo")
+      subject.color = "foo"
+      expect(subject.color).to eq("foo")
     end
 
     it "allows reading for :wheels" do
-      expect(car.wheels).to eq(4)
+      expect(subject.wheels).to eq(4)
     end
 
   end
@@ -44,7 +49,7 @@ describe 'Car' do
   describe '#full_name' do
     let(:honda) {Car.new(make: 'Honda', year: 2016, color: 'blue')}
     let(:car) {Car.new}
-    
+
     it "returns a string in the expected format" do
       expect(honda.full_name).to eq('2016 Honda (blue)')
     end
