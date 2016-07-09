2cc3d4be14af8edfeec29d6ed0a22b1202dc8c53
Mali Michael <malimccalla@gmail.com>
2016-07-09 16:20:29 +0100

    replace #before with #let

--- car_project/spec/car_spec.rb
+++ car_project/spec/car_spec.rb
@@ -4,46 +4,49 @@ describe 'Car' do
 
   describe 'attributes' do
 
-    before(:example) do
-      @car = Car.new
-    end
+    let(:car) { Car.new } #when car is used its calling the helper method car which runs that block of code
+    # let is bbetter than before for setting up instance variables
+    # before(:example) do
+    #   @car = Car.new
+    # end
 
     it "allows reading and writing for :make" do
-      @car.make = 'Test'
-      expect(@car.make).to eq('Test')
+      car.make = 'Test'
+      expect(car.make).to eq('Test')
     end
 
     it "allows reading and writing for :year" do
       #skip("Testing skipping this test.")
-      @car.year = 9999
-      expect(@car.year).to eq(9999)
+      car.year = 9999
+      expect(car.year).to eq(9999)
     end
 
     it "allows reading and writing for :color" do
-      @car.color = "foo"
-      expect(@car.color).to eq("foo")
+      car.color = "foo"
+      expect(car.color).to eq("foo")
     end
 
     it "allows reading for :wheels" do
-      expect(@car.wheels).to eq(4)
+      expect(car.wheels).to eq(4)
     end
 
   end
 
   describe '.colors' do
+    let(:c) {['blue', 'black', 'red', 'green']}
 
     it "it returns an array of color names" do
-      c = ['blue', 'black', 'red', 'green']
       expect(Car.colors).to match_array(c)
     end
 
   end
 
   describe '#full_name' do
-
+    let(:honda) {Car.new(make: 'Honda', year: 2016, color: 'blue')}
+    let(:car) {Car.new}
+    
     it "returns a string in the expected format" do
-      @honda = Car.new(make: 'Honda', year: 2016, color: 'blue')
-      expect(@honda.full_name).to eq('2016 Honda (blue)')
+      expect(honda.full_name).to eq('2016 Honda (blue)')
     end
 
     context "when initialized with no arguments" do
